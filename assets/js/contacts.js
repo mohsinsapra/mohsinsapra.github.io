57b9799fac921bbae7a994ca277aaf7be5c5a000
Muhammad Mohsin <mohsin.sapra@gmail.com>
2025-08-06 20:57:53 +0200

    master: updated portfolio

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 //Validation forms
 function validateForm(selector) {
     Array.from(document.querySelectorAll(selector)).forEach(item => {
@@ -18,38 +17,39 @@ function validateForm(selector) {
     });
 };
 
-validateForm('.js-form .form-field');
+validateForm('#contact-form .form-field');
 
-var form = document.querySelector('.js-form');
-var formName = '.js-form';
-
-form.addEventListener('submit', function(e){
-    submitForm(e, formName);
-});
-
-function submitForm(e, formName) {
-    e.preventDefault();
-    var name = $(formName + ' .js-field-name').val();
-    var email = $(formName + ' .js-field-email').val();
-    var message = $(formName + ' .js-field-message').val();
-
-    var formData = {
-        name: name,
-        email: email,
-        message: message
-    };
-
-    $.ajax({
-        type: "POST",
-        url: 'mail.php',
-        data: formData,
-        success: function () {
-            console.log('success');
-            //...
-        },
-        error: function () {
-            console.log('error');
-            //...
+// Formspree form handling
+var form = document.getElementById("contact-form");
+  
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("contact-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Thanks for your submission!";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form"
         }
-    });
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+
+if (form) {
+  form.addEventListener("submit", handleSubmit);
 }
